95bce085525e26f68d038bf24385a3434bd3e00b
零大魔王 <3296427001@qq.com>
2023-03-08 20:29:03 +0800

    feat: 第一次初始化

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Home() {
+  const navigate = useNavigate();
+  const goAboutPage = () => {
+    navigate('/about');
+  };
+
+  return (
+    <div>
+      <button className={'text-6xl'} onClick={goAboutPage}>点击跳转到about页面</button>
+    </div>
+  );
+}
+
+export default Home;
